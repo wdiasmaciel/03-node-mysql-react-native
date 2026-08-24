9a50a4ca528a45cc3440d905bf2a8a2edd0f76e0
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-08-24 06:53:31 +0000

    Exemplo: Node, MySQL e React Native

--- meu-app/App.tsx
+++ meu-app/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Principal from './components/Principal';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Principal />
       <StatusBar style="auto" />
     </View>
   );
